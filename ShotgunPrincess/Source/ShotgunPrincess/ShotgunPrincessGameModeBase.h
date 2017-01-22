ff957c1a6de58045092de815f174960b27b5ba41
ZackMisso <zack441@mac.com>
2017-01-21 20:58:51 -0600

    Initial Project Commit

---
+++ ShotgunPrincess/Source/ShotgunPrincess/ShotgunPrincessGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "ShotgunPrincessGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOTGUNPRINCESS_API AShotgunPrincessGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
