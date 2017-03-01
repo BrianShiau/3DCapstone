30842d286c1a864d9d53c07b48e24d04f2fcc02e
ZackMisso <zack441@mac.com>
2017-03-01 15:03:58 -0600

    Added Utility Function to Better Aim Bullets

---
+++ ShotgunPrincess/Source/ShotgunPrincess/FractalOuthaulBFL.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ShotgunPrincess.h"
+#include "FractalOuthaulBFL.h"
+#include "Kismet/KismetMathLibrary.h"
+
+FRotator UFractalOuthaulBFL::RotationToHit(AActor* firer, AActor* target, float bulletVelocity)
+{
+    // firer's location
+    FVector Q = firer->GetActorLocation();
+    // target's location
+    FVector P = target->GetActorLocation();
+    // target's velocity
+    FVector Vel = target->GetVelocity();
+    
+    float t1 = 0.0f;
+    float t2 = 0.0f;
+    float t = 0.0f;
+    
+    float a = FVector::DotProduct(Vel, Vel) - bulletVelocity * bulletVelocity;
+    float b = 2.0f * FVector::DotProduct(Q - P, Vel);
+    float c = (Q - P).SizeSquared();
+    
+    float sqrt = UKismetMathLibrary::Sqrt(b * b - 4 * a * c);
+    
+    if (a == 0.0f)
+    {
+        t = 0.0f;
+    }
+    else
+    {
+        t1 = (b + sqrt) / (2.0f * a);
+        t2 = (b - sqrt) / (2.0f * a);
+    
+        t = (t1 > t2 && t2 > 0.0f) ? t2 : t1;
+    }
+    
+    GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::Printf(TEXT("Velocity %f %f Time %f"), Vel[0], Vel[1], t));
+    
+    return UKismetMathLibrary::FindLookAtRotation(Q, P - t * Vel);
+}
+
+
---
+++ ShotgunPrincess/Source/ShotgunPrincess/FractalOuthaulBFL.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "FractalOuthaulBFL.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHOTGUNPRINCESS_API UFractalOuthaulBFL : public UBlueprintFunctionLibrary
+{
+public:
+	GENERATED_BODY()
+	
+    // returns the rotation necessary to hit a moving target
+	UFUNCTION(BlueprintCallable, Category="AI")
+    static FRotator RotationToHit(AActor* firer, AActor* target, float bulletVelocity);
+	
+};
